515beae5e6b8190e645e525a28be4025ad6455a2
Marcos Duarte <marcosduarte1994@gmail.com>
2022-12-04 22:34:15 -0300

    test: ensure SignupController return 400 if no name is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,6 +1,6 @@
 import { SignUpController } from './signup'
 
-describe('signup controller ', () => {
+describe('signup controller', () => {
   test('Should return 400 if no name is provided', () => {
     const sut = new SignUpController()
     const httpRequest = {
